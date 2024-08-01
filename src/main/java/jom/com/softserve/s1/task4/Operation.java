75b8242f1d3ecf58b3d4497cb70f0d533b3ca8ed
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-01 13:40:57 +0300

    Task 4 Rectangle

--- src/main/java/jom/com/softserve/s1/task4/Operation.java
+++ src/main/java/jom/com/softserve/s1/task4/Operation.java
@@ -2,12 +2,20 @@ package jom.com.softserve.s1.task4;
 
 class Operation {
 	public static int squareRectangle(int a, int b) {
-		//your code
-		return 0;
+		if (a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("both arguments should be more than zero");
+		}
+		return a * b;
 	}
 
 	public static int trySquareRectangle(int a, int b) {
-		//your code
-		return 0;
+		try {
+//			int perimeter = 2 * (a + b);
+//			return ((perimeter - 2 * a) * (perimeter - 2 * b)) / 4;
+			return squareRectangle(a, b);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return -1;
+		}
 	}
 }
