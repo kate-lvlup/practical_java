f9dec4d48bdb2cafe12e1a4c84e3b3d1a800e6fa
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-01 11:28:48 +0300

    Task 2 Class Point

--- src/main/java/jom/com/softserve/s1/task2/Point.java
+++ src/main/java/jom/com/softserve/s1/task2/Point.java
@@ -1,5 +1,30 @@
 package jom.com.softserve.s1.task2;
 
 public class Point {
-	//your code
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getXYPair() {
+        int[] arr = new int[2];
+        arr[0] = x;
+        arr[1] = y;
+        return arr;
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+    public double distance() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
 }
