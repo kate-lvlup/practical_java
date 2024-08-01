4fb498fbf9629fe86c15e9fa4c7b1ce79bbf835b
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-01 10:05:14 +0300

    Task 1 Class Circle

--- src/main/java/jom/com/softserve/s1/task1/Circle.java
+++ src/main/java/jom/com/softserve/s1/task1/Circle.java
@@ -1,14 +1,26 @@
 package jom.com.softserve.s1.task1;
 
 class Circle {
-	private double radius;
+    private double radius;
 
-	public Circle(double radius) {
-		this.radius = radius;
-	}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
-	public void draw() {
-		// Draw Circle with default color and radius
-	}
-	//your code
+    public void draw() {
+        System.out.println("Radius " + radius);
+    }
+
+    public void draw(String color) {
+        System.out.println("Color " + color + " radius " + radius);
+    }
+
+    public void draw(float scale) {
+        System.out.println("Scale " + scale + " radius " + radius * scale);
+    }
+
+    public void draw(String color, float scale) {
+        System.out.println("Color " + color + " scale " + scale + " scaled radius " + radius * scale);
+    }
+
 }
