8e34921e6ccf132038def75ffe4b7f9785ac48a7
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-01 16:08:26 +0300

    Task7 created class InsufficientAmountException

--- src/main/java/jom/com/softserve/s1/task7/CheckingAccount.java
+++ src/main/java/jom/com/softserve/s1/task7/CheckingAccount.java
@@ -1,23 +1,41 @@
 package jom.com.softserve.s1.task7;
 
+class InsufficientAmountException extends Exception {
+    private double amount;
+
+    public InsufficientAmountException(double amount) {
+        super("Sorry, but you are short $" + amount);
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
+
 public class CheckingAccount {
-	private double balance;
-	private int number;
+    private double balance;
+    private int number;
 
-	public CheckingAccount(int number) {
-		this.number = number;
-	}
+    public CheckingAccount(int number) {
+        this.number = number;
+    }
 
-	public void deposit(double amount) {
-		balance += amount;
-	}
+    public void deposit(double amount) {
+        balance += amount;
+    }
 
-//	public void withdraw(double amount) throws InsufficientAmountException {
-//		if (amount <= balance) {
-//			balance -= amount;
-//		} else {
-//			double needs = amount - balance;
-//			 new InsufficientAmountException(needs);
-//		}
-//	}
+    public void withdraw(double amount) throws InsufficientAmountException {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            double needs = amount - balance;
+            throw new InsufficientAmountException(needs);
+        }
+    }
 }
