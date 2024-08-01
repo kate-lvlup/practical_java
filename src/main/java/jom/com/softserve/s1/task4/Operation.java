34c6c09902cc5e9673566e35417fb058a91b8bc3
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-01 16:42:41 +0300

    Task 4 Update

--- src/main/java/jom/com/softserve/s1/task4/Operation.java
+++ src/main/java/jom/com/softserve/s1/task4/Operation.java
@@ -10,11 +10,8 @@ class Operation {
 
 	public static int trySquareRectangle(int a, int b) {
 		try {
-//			int perimeter = 2 * (a + b);
-//			return ((perimeter - 2 * a) * (perimeter - 2 * b)) / 4;
 			return squareRectangle(a, b);
 		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
 			return -1;
 		}
 	}
